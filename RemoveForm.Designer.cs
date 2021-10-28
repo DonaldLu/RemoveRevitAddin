417b6487155dce1e909280f505d05e40c5b27f79
Donald Lu <lugenius2001@gmail.com>
2021-10-28 16:22:12 +0800

    移除Revit外掛

---
+++ RemoveForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RemoveRevitAddin
+{
+    partial class RemoveForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RemoveForm));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sureBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "2018",
+            "2019",
+            "2020"});
+            this.checkedListBox1.Location = new System.Drawing.Point(16, 33);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(251, 70);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "請選擇要移除的版本";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "中興自動化",
+            "捷運規範校核"});
+            this.checkedListBox2.Location = new System.Drawing.Point(16, 138);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(251, 48);
+            this.checkedListBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "請選擇要移除的產品";
+            // 
+            // sureBtn
+            // 
+            this.sureBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.sureBtn.Location = new System.Drawing.Point(16, 204);
+            this.sureBtn.Name = "sureBtn";
+            this.sureBtn.Size = new System.Drawing.Size(75, 36);
+            this.sureBtn.TabIndex = 2;
+            this.sureBtn.Text = "確定";
+            this.sureBtn.UseVisualStyleBackColor = true;
+            this.sureBtn.Click += new System.EventHandler(this.sureBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelBtn.Location = new System.Drawing.Point(192, 204);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 36);
+            this.cancelBtn.TabIndex = 2;
+            this.cancelBtn.Text = "取消";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // RemoveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(284, 255);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.sureBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(300, 294);
+            this.MinimumSize = new System.Drawing.Size(300, 294);
+            this.Name = "RemoveForm";
+            this.Text = "移除Revit外掛";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sureBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
+
---
+++ RemoveForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RemoveRevitAddin
+{
+    public partial class RemoveForm : Form
+    {
+        public RemoveForm()
+        {
+            InitializeComponent(); CenterToParent(); // 置中
+        }
+        // 確定
+        private void sureBtn_Click(object sender, EventArgs e)
+        {
+            foreach (var versionItem in checkedListBox1.CheckedItems)
+            {
+                foreach (var productItem in checkedListBox2.CheckedItems)
+                {
+                    string fileName = string.Empty;
+                    if (productItem.ToString().Equals("中興自動化"))
+                    {
+                        fileName = "Sinotech";
+                    }
+                    else if (productItem.ToString().Equals("捷運規範校核"))
+                    {
+                        fileName = "Sino_Station";
+                    }
+                    string directoryPath = @"C:\ProgramData\Autodesk\Revit\Addins\" + versionItem.ToString() + @"\" + fileName; // 資料夾
+                    string addinPath = @"C:\ProgramData\Autodesk\Revit\Addins\" + versionItem.ToString() + @"\" + fileName + ".addin"; // addin
+                    // 檢查是否有此addin, 有的話則移除
+                    bool fileExists = File.Exists(addinPath);
+                    if (fileExists == true)
+                    {
+                        try
+                        {
+                            File.Delete(addinPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message + "\n" + ex.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("找不到檔案。");
+                    }
+                    // 檢查是否有此資料夾, 有的話則移除
+                    bool directoryExists = Directory.Exists(directoryPath);
+                    if (directoryExists == true)
+                    {
+                        DeleteFolder(directoryPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("無此資料夾。");
+                    }
+                }
+            }
+            Close(); // 移除後關閉
+        }
+        // 完整刪除資料夾
+        public static void DeleteFolder(string dir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetFileSystemEntries(dir))
+                {
+                    try
+                    {
+                        if (File.Exists(d))
+                        {
+                            // 如果是唯獨則先修改
+                            FileInfo fi = new FileInfo(d);
+                            if (fi.Attributes.ToString().IndexOf("ReadOnly") != -1)
+                            {
+                                fi.Attributes = FileAttributes.Normal;
+                            }
+                            File.Delete(d); // 直接删除其中的文件
+                        }
+                        else
+                        {
+                            DeleteFolder(d); // 遞迴刪除子資料夾
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message + "\n" + ex.ToString());
+                    }
+                }
+                Directory.Delete(dir); // 刪除已清空的資料夾
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n" + ex.ToString());
+            }
+        }
+        // 取消
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Close(); // 關閉
+        }
+    }
+}
